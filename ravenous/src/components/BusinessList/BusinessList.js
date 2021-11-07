d601b615ef5e9b2aa3b3126fe37799e7123d156d
Matthijs Boet <m.boet@chello.nl>
2021-11-07 21:27:58 +0100

    changes to ravenous

---
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Business from '../Business/Business';
+import './BusinessList.css';
+
+export default class BusinessList extends Component {
+	render() {
+		return (
+			<div className='BusinessList'>
+				{this.props.businesses.map(business => {
+					return <Business business={business} />;
+				})}
+			</div>
+		);
+	}
+}
